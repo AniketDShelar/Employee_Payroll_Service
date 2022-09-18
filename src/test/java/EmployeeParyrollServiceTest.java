afae7b239ba516e5edd49fa564b14f871896fe06
AniketDShelar <theaniketshelar@gmail.com>
2022-09-18 12:52:53 +0530

    store employee to file

--- src/test/java/EmployeeParyrollServiceTest.java
+++ src/test/java/EmployeeParyrollServiceTest.java
@@ -21,21 +21,4 @@ public class EmployeeParyrollServiceTest {
         long count = employeeParyrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
         Assertions.assertEquals(3,count);
     }
-//    @Test
-//    void givenThreeEmployeesAfterInConsoleWritingShouldReturnEntryCount(){
-//        Employee employee1 = new Employee(1,"Amit",20000);
-//        Employee employee2 = new Employee(2,"Neha",25000);
-//        Employee employee3 = new Employee(3,"akash",30000);
-//
-//        List<Employee> list = new ArrayList<>();
-//        list.add(employee1);
-//        list.add(employee2);
-//        list.add(employee3);
-//
-//        EmployeeParyrollService employeeParyrollService = new EmployeeParyrollService(list);
-//        employeeParyrollService.writeData(EmployeeParyrollService.IOService.CONSOLE_IO);
-//        employeeParyrollService.readData(EmployeeParyrollService.IOService.CONSOLE_IO);
-//        long count = employeeParyrollService.countEntries(EmployeeParyrollService.IOService.CONSOLE_IO);
-//        Assertions.assertEquals(3,count);
-//    }
 }
